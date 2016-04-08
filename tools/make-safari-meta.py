35d37b5840e53bcdaf11718adaa87e4eccbd22b3
Conor Randall <coolo1mc@gmail.com>
2016-04-08 17:39:34 +1000

    fix safari

--- tools/make-safari-meta.py
+++ tools/make-safari-meta.py
@@ -50,9 +50,9 @@ for alpha2 in sorted(os.listdir(locale_dir)):
 
 # update Info.plist
 proj_dir = pj(os.path.split(os.path.abspath(__file__))[0], '..')
-chromium_manifest = pj(proj_dir, 'platform', 'chromium', 'manifest.json')
+safari_manifest = pj(proj_dir, 'platform', 'safari', 'manifest.json')
 
-with open(chromium_manifest, encoding='utf-8') as m:
+with open(safari_manifest, encoding='utf-8') as m:
     manifest = json.load(m)
 
 manifest['buildNumber'] = int(time())
